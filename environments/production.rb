b3ed1d7d64e10c853b9d6ce3d6023d4f721ddc84
Ches Martin <ches@whiskeyandgrits.net>
2011-10-10 02:45:48 +0700

    App python packages need to build native extensions

--- environments/production.rb
+++ environments/production.rb
@@ -2,8 +2,9 @@ name 'production'
 description 'The production environment. Hosting contact: james@ready.co.th'
 
 cookbook_versions(
-  'apt'         => '1.1.2',
-  'chef-client' => '1.0.2',
-  'ubuntu'      => '0.99.0'
+  'apt'             => '1.1.2',
+  'build-essential' => '1.0.0',
+  'chef-client'     => '1.0.2',
+  'ubuntu'          => '0.99.0'
 )
 
--- roles/app.rb
+++ roles/app.rb
@@ -3,6 +3,7 @@ description 'Base role for a system to run the Barcamp Django application'
 
 run_list(
   'role[base-ubuntu]',
+  'recipe[build-essential]',    # For pip packages to build native extensions
   'recipe[barcampbkk]'
 )
 
