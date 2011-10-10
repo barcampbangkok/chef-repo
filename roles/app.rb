55e4d016ba9003cf58bac82bb4373f45cc87f1b4
Ches Martin <ches@whiskeyandgrits.net>
2011-10-10 12:34:33 +0700

    Don't include Postgres server in app role
    It should be a discrete entry in a node's run list to meet the
    application cookbook's expectatations.

--- roles/app.rb
+++ roles/app.rb
@@ -3,7 +3,6 @@ description 'Base role for a system to run the Barcamp Django application'
 
 run_list(
   'role[base-ubuntu]',
-  'role[postgresql-server]',
   'recipe[build-essential]',    # For pip packages to build native extensions
   'recipe[barcampbkk]',
   'recipe[application]'
